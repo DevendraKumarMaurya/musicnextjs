39e9576aeb1b4ae1fc2670e3f3f86db2c0da44fb
Devendra Kumar Maurya <jatin08072004@gmail.com>
2025-08-17 22:19:43 +0530

    feat: Update ESLint configuration and improve component imports and structure

--- src/components/ui/navbar-menu.tsx
+++ src/components/ui/navbar-menu.tsx
@@ -2,15 +2,6 @@
 import React from "react";
 import { motion } from "motion/react";
 
-const transition = {
-  type: "spring",
-  mass: 0.5,
-  damping: 11.5,
-  stiffness: 100,
-  restDelta: 0.001,
-  restSpeed: 0.001,
-};
-
 export const MenuItem = ({
   setActive,
   active,
@@ -25,7 +16,7 @@ export const MenuItem = ({
   return (
     <div onMouseEnter={() => setActive(item)} className="relative ">
       <motion.p
-        transition={{ duration: 0.3 }}
+        transition={{ type: "spring", duration: 0.3 }}
         className="cursor-pointer text-black hover:opacity-[0.9] dark:text-white"
       >
         {item}
@@ -34,17 +25,39 @@ export const MenuItem = ({
         <motion.div
           initial={{ opacity: 0, scale: 0.85, y: 10 }}
           animate={{ opacity: 1, scale: 1, y: 0 }}
-          transition={transition}
+          transition={{
+            type: "spring",
+            mass: 0.5,
+            damping: 11.5,
+            stiffness: 100,
+            restDelta: 0.001,
+            restSpeed: 0.001,
+          }}
         >
           {active === item && children && (
             <div className="absolute top-[calc(100%_+_1.2rem)] left-1/2 transform -translate-x-1/2 pt-4">
               <motion.div
-                transition={transition}
-                layoutId="active" // layoutId ensures smooth animation
+                transition={{
+                  type: "spring",
+                  mass: 0.5,
+                  damping: 11.5,
+                  stiffness: 100,
+                  restDelta: 0.001,
+                  restSpeed: 0.001,
+                }}
+                layoutId="active"
                 className="bg-white dark:bg-black backdrop-blur-sm rounded-2xl overflow-hidden border border-black/[0.2] dark:border-white/[0.2] shadow-xl"
               >
                 <motion.div
-                  layout // layout ensures smooth animation
+                  layout
+                  transition={{
+                    type: "spring",
+                    mass: 0.5,
+                    damping: 11.5,
+                    stiffness: 100,
+                    restDelta: 0.001,
+                    restSpeed: 0.001,
+                  }}
                   className="w-max h-full p-4"
                 >
                   {children}
